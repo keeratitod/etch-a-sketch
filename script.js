4c0a83ba645f856e178d8878e0099fdff9dad661
keerati tod <keerati.tod@gmail.com>
2022-01-02 13:42:51 +0700

    add index.html script.js style.css basic version

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("container");
+let cell;
+
+function makeGrid(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeGrid(16, 16);
+
+const hover = document.querySelector("div");
+hover.addEventListener("mouseover", function(event) {
+  event.target.classList.add("hover");
+});
+
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", function() {
+  console.log("clear called");
+  document.getElementById("container").innerHTML = "";
+  makeGrid(16, 16);
+});
