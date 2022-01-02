6d79ab45fd25d15cf07a555e572a59030fe388e3
keerati tod <keerati.tod@gmail.com>
2022-01-02 14:05:37 +0700

    add clear and resize button

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const container = document.getElementById("container");
 let cell;
 
 function makeGrid(rows, cols) {
+
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
   };
+
 };
 
 makeGrid(16, 16);
@@ -22,5 +25,11 @@ const clear = document.querySelector(".clear");
 clear.addEventListener("click", function() {
   console.log("clear called");
   document.getElementById("container").innerHTML = "";
-  makeGrid(16, 16);
+  let newGrid = prompt("what size?");
+  if (newGrid > 100) {
+    newGrid = 100;
+  }
+  rows = newGrid;
+  cols = newGrid;
+  makeGrid(rows, cols);
 });
